c7295eb3a230cf22418365c88531d6407447ca8d
Mike Izbicki <mike@izbicki.me>
2022-02-21 07:54:00 -0800

    add comments

--- load_tweets.sh
+++ load_tweets.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # list all of the files that will be loaded into the database
 # for the first part of this assignment, we will only load a small test zip file with ~10000 tweets
 # but we will write are code so that we can easily load an arbitrary number of files
@@ -5,10 +7,12 @@ files='
 test-data.zip
 '
 
+echo 'load normalized'
 for file in $files; do
     # call the load_tweets.py file to load data into pg_normalized
 done
 
+echo 'load denormalized'
 for file in $files; do
     # use SQL's COPY command to load data into pg_denormalized
 done
