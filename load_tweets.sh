9cc871e62334127f1325aefa9999f44294b570b1
Mike Izbicki <mike@izbicki.me>
2022-02-20 23:35:12 -0800

    add note

--- load_tweets.sh
+++ load_tweets.sh
@@ -1,3 +1,6 @@
+# list all of the files that will be loaded into the database
+# for the first part of this assignment, we will only load a small test zip file with ~10000 tweets
+# but we will write are code so that we can easily load an arbitrary number of files
 files='
 test-data.zip
 '
